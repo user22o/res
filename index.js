f2f13d0ac55e1ba299d27f3d56543d6c83f2ca21
hussein <husseinkawtharany2006@gmail.com>
2022-08-11 23:58:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var exp = require("express")
+var app = exp()
+
+app.listen(process.env.PORT||8008,()=>{
+    console.log("listening\n");
+})
+
+app.get("/",(req,res)=>{
+    res.send({"a":123});
+})
+app.post("/",(req,res)=>{
+
+    res.send({"a":2});
+})
+console.log("star");
